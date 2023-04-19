14f2dcbbb4f544b69df36372aa2553eaf56251a7
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2023-04-20 05:54:11 +0700

    membuat repository & service : menampilkan todolist

---
+++ repository/TodolistRepositoryImpl.java
@@ -0,0 +1,29 @@
+package repository;
+
+import entity.Todolist;
+
+/**
+ * Membuat repository & service : menampilkan todolist
+ */
+public class TodolistRepositoryImpl implements TodolistRepository {
+
+    private Todolist[] data = new Todolist[10];
+
+    @Override
+    public Todolist[] getAll() {
+        return data;
+    }
+
+    @Override
+    public void add(Todolist todolist) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'add'");
+    }
+
+    @Override
+    public void remove(Integer number) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'remove'");
+    }
+    
+}
